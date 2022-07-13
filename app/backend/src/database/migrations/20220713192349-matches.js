7a0a13f2dd2b7a06383d87ecddb4512edba14aa7
Larissa Araujo <larissa.jvsa@gmail.com>
2022-07-13 16:44:29 -0300

    feat: :fire: creating migrations for users, teams and matches tables

---
+++ app/backend/src/database/migrations/20220713192349-matches.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
