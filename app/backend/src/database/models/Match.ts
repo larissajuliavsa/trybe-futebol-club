82d0cb0e627b2d532822dd0a029a2a2727c3cc74
Larissa Araujo <larissa.jvsa@gmail.com>
2022-07-13 19:31:06 -0300

    feat: :fire: models: match, team and user

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,51 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Match extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: boolean;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Team.belongsTo(Match, { foreignKey: 'homeTeam', as: 'id' });
+Team.belongsTo(Match, { foreignKey: 'awayTeam', as: 'id' });
+
+export default Match;
