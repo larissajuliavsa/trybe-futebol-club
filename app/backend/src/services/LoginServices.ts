d9c107505858c67cf6635a78836d897854078abe
Larissa Araujo <larissa.jvsa@gmail.com>
2022-07-20 19:20:10 -0300

    fix: :ambulance: deleting comments

--- app/backend/src/services/LoginServices.ts
+++ app/backend/src/services/LoginServices.ts
@@ -1,12 +1,6 @@
-// import { Request, Response, NextFunction } from 'express';
 import { ILogin, ILoginService, ILoginModel } from '../interface/Login';
 import Token from '../utils/Token';
 import Bcrypt from '../utils/Bcrypt';
-
-// const MSG_NULL = 'All fields must be filled';
-// const MSG_INCORRECT = 'Incorrect email or password';
-// const BAD_REQUEST = 400;
-// const UNAUTHORIZED = 401;
 
 export default class LoginService implements ILoginService {
   token: Token;
@@ -20,12 +14,8 @@ export default class LoginService implements ILoginService {
 
   public async getLogin(login: ILogin):Promise<string> {
     const data = await this.model.getLogin(login.email);
-    // if (!data) throw new Error('Sorry, I didnt find it');
-
-    // await this.bcrypt.compare(login.password, data.password);
-    // if (!crypt) throw new Error('Sorry, I didnt find it');
-
     const loginToken = this.token.getToken(data);
+
     return loginToken;
   }
 }
